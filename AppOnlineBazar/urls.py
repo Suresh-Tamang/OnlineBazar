20608dd044432fa0c14f109a9c64daa9609a2ee3
Suresh Tamang <sureshjimba3333@gmail.com>
2023-09-06 19:03:06 +0545

    design initialized

--- AppOnlineBazar/urls.py
+++ AppOnlineBazar/urls.py
@@ -2,4 +2,6 @@ from django.urls import path,include
 from .import views
 urlpatterns = [
     path('',views.Index,name="index"),
+    path('register/',views.Register,name="register"),
+    path('login',views.Login,name='login'),
 ]
--- AppOnlineBazar/views.py
+++ AppOnlineBazar/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def Index(request):
     return render(request,'AppOnlineBazar/index.html')
+
+
+def Register(request):
+    return render(request,'AppOnlineBazar/register.html')
+
+def Login(request):
+    return render(request,'AppOnlineBazar/login.html')
+
